ee47063f7474c798c580ed6a7fd8ab7947b164e9
bashar-the-coder <mat197194@gmail.com>
2022-11-27 10:28:08 +0600

    crawl project start

---
+++ crproject/crproject/spiders/books_crawl.py
@@ -0,0 +1,20 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BooksCrawlSpider(CrawlSpider):
+    name = 'books_crawl'
+    allowed_domains = ['books.toscrape.com'] # dont add https and remove backslash 
+    start_urls = ['https://books.toscrape.com/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        item = {}
+        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        #item['name'] = response.xpath('//div[@id="name"]').get()
+        #item['description'] = response.xpath('//div[@id="description"]').get()
+        return item
